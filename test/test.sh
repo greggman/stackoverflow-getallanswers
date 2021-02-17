48f7750c03e06be6490bed8491854442ba159c7b
Gregg Tavares <github@greggman.com>
2021-02-17 17:56:20 +0900

    fix test folder

--- test/test.sh
+++ test/test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python ../getallanswers.py --posts=test-Posts.xml --posthistory=test-Posthistory.xml --users=test-Users.xml --userid="128511" --out=answers-by-user.json
+python ../getallanswers.py --posts=Posts.xml --posthistory=Posthistory.xml --users=Users.xml --userid="128511" --out=answers-by-user.json
